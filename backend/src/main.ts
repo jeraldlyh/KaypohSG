a0e78d6ee4abf00e8ab4fb9dc963abeaf6011f5f
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-08-11 23:47:26 +0800

    build(auth): setup dependencies

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,34 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import * as firebase from 'firebase-admin';
+import { AppModule } from './app/app.module';
+import { WEB_URL } from './common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService: ConfigService = app.get(ConfigService);
+  const serviceAccount: firebase.ServiceAccount = {
+    projectId: configService.get<string>('FIREBASE_PROJECT_ID'),
+    privateKey: configService
+      .get<string>('FIREBASE_PRIVATE_KEY')
+      .replace(/\\n/g, '\n'),
+    clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+  };
+
+  firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+  });
+  firebase.firestore().settings({ ignoreUndefinedProperties: true });
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+  app.enableCors({
+    credentials: true,
+    origin: WEB_URL,
+  });
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
