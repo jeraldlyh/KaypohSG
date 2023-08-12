aa75c08bf32c13b2080b10cae200f55a7a713cf8
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-08-12 15:20:09 +0800

    style(web): make navbar sticky

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -37,7 +37,7 @@ export const NavBar = ({ onLogout }: IProps): JSX.Element => {
   /*                                   RENDER                                   */
   /* -------------------------------------------------------------------------- */
   return (
-    <div className="navbar rounded-box flex justify-between bg-base-200 pl-5 pr-10 shadow-xl md:pl-6">
+    <div className="navbar rounded-box sticky top-3 z-10 flex justify-between bg-base-200 pl-5 pr-10 shadow-xl md:pl-6">
       <a className="btn btn-ghost gap-0 text-lg font-bold normal-case md:text-xl">
         Kaypoh
         <span className="text-custom-red">SG</span>
