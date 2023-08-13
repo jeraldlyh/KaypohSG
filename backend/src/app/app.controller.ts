8fe7d17bca96621f1dbf17217f243fc6266bc2fd
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-08-13 14:33:17 +0800

    fix(backend): expose health check endpoint

--- backend/src/app/app.controller.ts
+++ backend/src/app/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from '../common';
 
 @Controller()
 export class AppController {
   @Get()
+  @Public()
   getHello(): string {
     return 'hello world';
   }
