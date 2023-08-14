e7aea237a5bbb850446dd71051eab581937ee4c7
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-08-15 00:03:31 +0800

    chore: add note for future improvements

--- web/src/hooks/useContribution.ts
+++ web/src/hooks/useContribution.ts
@@ -6,6 +6,7 @@ export const useContribution = () => {
   const { data, error, isLoading, mutate } = useSWR(
     SERVER_ROUTES.CONTRIBUTION.GET_ALL,
     ContributionService.getAll,
+    // NOTE: Able to add refresh interval to poll for new updates
   );
 
   return { data, error, isLoading, mutate };
